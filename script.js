f160bf15b8996da18ebcb54e2ced563925f215b4
Philipp Wüthrich <phiwut@users.noreply.github.com>
2018-02-26 14:04:47 +0100

    It now displays text in a carousel

--- script.js
+++ script.js
@@ -6,6 +6,24 @@ var title;
 window.addEventListener("blur", function () {
   title = document.title;
   document.title = message;
+  
+	var i = 0;
+	
+	function myLoop () {
+		setTimeout(function () {
+			document.title = message.substr(i, message.length - i) + " " + message.substr(0, i);
+			if(i + 1 == message.length) {
+				i = 0;
+			} else {
+				i++;
+			}
+			myLoop();
+		}, 500)
+	}
+	
+	if(!window.blur){
+		myLoop();
+	}
 })
 
 window.addEventListener("focus", function () {
